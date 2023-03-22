458293e5b7c85e6435da0004d4d141149bb41508
Toprak2 <115584002+Toprak2@users.noreply.github.com>
2023-03-22 22:16:14 +0300

    Save user input Give info about their form to user

--- getFormData.php
+++ getFormData.php
@@ -1,3 +1,33 @@
-<?php
-echo $_POST["first-name"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><?php
+function test_input($data){
+    $data = trim($data);
+    $data=stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$first_name =test_input( $_POST["first-name"]);
+$last_name =test_input( $_POST["last-name"]);
+$email = test_input($_POST["email"]);
+$gender =test_input($_POST["gender"]);
+$feedback =test_input($_POST["feedback"]);
 ?>
+    <h1>Thank you for your feedback</h1>
+    <h2>Your feedback:</h2>
+    <div>
+    <?php
+    echo $feedback;
+    ?>
+    </div>
+<a href="./index.html">Home</a>
+</body>
+</html>
+
